f5e5132cc1f48e55aaff5c8ccaa3882f469640e4
William Greenlee <greenlee04@gmail.com>
2022-12-03 16:27:14 -0500

    Solved Day 1 Challenges

---
+++ 2022/Day1/Day1.py
@@ -0,0 +1,21 @@
+totals = []
+index = [0]
+
+
+def stuff(line, index, totals):
+    if (line == "\n"):
+        index[0] += 1
+    else:
+        if (index[0] >= len(totals)):
+            totals.append(int(line))
+        else:
+            totals[index[0]] += int(line)
+
+
+for line in open("C:/Users/Green/git/AdventOfCode/2022/Day1/input1.txt"):
+    stuff(line, index, totals)
+
+
+totals = sorted(totals, reverse=True)
+
+print(totals[0]+totals[1]+totals[2])
